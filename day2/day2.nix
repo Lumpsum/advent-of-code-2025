106b542b17ae2060e5b3c35b83da40d050368a37
Lumpsum <vergunstje@hotmail.com>
2025-12-02 22:49:51 +0000

    feat: day2 part 2

--- day2/day2.nix
+++ day2/day2.nix
@@ -1,4 +1,9 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
 let
+  lib = pkgs.lib;
+
   isSame =
     input:
     let
@@ -10,9 +15,46 @@ let
       result = if right == left then input else 0;
     in
     result;
+
+  isInvalidId =
+    id:
+    let
+      len = builtins.stringLength id;
+      half = builtins.floor (len / 2);
+      nGramOptions = lib.range 1 half;
+      isInvalid = builtins.any (x: x == true) (map (n: allSame (nGrams id n)) nGramOptions);
+    in
+    isInvalid;
+
+  nGrams =
+    s: n:
+    let
+      len = builtins.stringLength s;
+
+      splitRec =
+        index:
+        if index >= len then
+          [ ]
+        else
+          let
+            chunk = builtins.substring index n s;
+          in
+          [ chunk ] ++ splitRec (index + n);
+    in
+    splitRec 0;
+
+  allSame =
+    l:
+    let
+      first = builtins.elemAt l 0;
+    in
+    builtins.all (x: x == first) l;
 in
 {
   inherit
     isSame
+    allSame
+    nGrams
+    isInvalidId 
     ;
 }
--- day2/default.nix
+++ day2/default.nix
@@ -4,26 +4,48 @@
 let
   lib = pkgs.lib;
   utils = import ../utils.nix { inherit pkgs; };
-  day2 = import ./day2.nix;
+  day2 = import ./day2.nix { inherit pkgs; };
 
   partOne =
     p:
     let
       input = utils.list.createList p ",";
 
-      rangeLists = map (
-        item:
-        lib.range (lib.strings.toIntBase10 (builtins.elemAt item 0)) (
-          lib.strings.toIntBase10 (builtins.elemAt item 1)
-        )
-      ) (map (item: lib.strings.splitString "-" item) input);
+      rangeLists = lib.flatten (
+        map (
+          item:
+          lib.range (lib.strings.toIntBase10 (builtins.elemAt item 0)) (
+            lib.strings.toIntBase10 (builtins.elemAt item 1)
+          )
+        ) (map (item: lib.strings.splitString "-" item) input)
+      );
 
-      valid = map (item: map (innerItem: (day2.isSame innerItem)) item) rangeLists;
-      result = builtins.foldl' utils.math.sum 0 (map (item: builtins.foldl' utils.math.sum 0 item) valid);
+      valid = map (innerItem: (day2.isSame innerItem)) rangeLists;
     in
-    result;
+    builtins.foldl' utils.math.sum 0 valid;
+
+  partTwo =
+    p:
+    let
+      input = utils.list.createList p ",";
+
+      rangeLists = lib.flatten (
+        map (
+          item:
+          lib.range (lib.strings.toIntBase10 (builtins.elemAt item 0)) (
+            lib.strings.toIntBase10 (builtins.elemAt item 1)
+          )
+        ) (map (item: lib.strings.splitString "-" item) input)
+      );
+
+      invalidIds = builtins.filter (x: day2.isInvalidId (builtins.toString x) == true) rangeLists;
+    in
+    builtins.foldl' utils.math.sum 0 invalidIds;
 in
 {
   examplePartOne = partOne ./example.txt;
   partOne = partOne ../inputs/day2/input.txt;
+
+  examplePartTwo = partTwo ./example.txt;
+  partTwo = partTwo ../inputs/day2/input.txt;
 }
